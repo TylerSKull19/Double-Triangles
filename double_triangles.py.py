2baf36f76095126b58b18298b9b7a2ed715e29e4
TylerSKull19 <tyler.j.smith051902@gmail.com>
2021-05-08 16:33:20 -0400

    Add files via upload

---
+++ double_triangles.py.py
@@ -0,0 +1,16 @@
+# Tyler Smith
+# CSC 101. Lab 1 
+# 08/26/2020
+import turtle
+turtle.showturtle()
+turtle.shape(name="turtle")
+turtle.pensize=10
+turtle.goto(x=100, y=200)
+turtle.goto(x=200, y=0)
+turtle.right(angle=180)
+turtle.forward(distance=200)
+turtle.goto(x=200, y=400)
+turtle.forward(distance=200)
+turtle.goto(x=100,y=200)
+
+
